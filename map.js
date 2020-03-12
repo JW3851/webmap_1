f93e9a9ff614b333384e7af851d21db39702ddc5
JW3851 <62093012+JW3851@users.noreply.github.com>
2020-03-12 06:01:32 -0400

    Update map.js

--- map.js
+++ map.js
@@ -44,3 +44,17 @@ map.addControl(geolocate, 'top-left')
 geolocate.on('geolocate', function(event) {
     console.log(event.coords)
 })
+
+geolocate.on('geolocate', function(event) {
+
+    // create new variables to store the attributes we're interested in from the event
+    let lng = event.coords.longitude
+    let lat = event.coords.latitude
+
+    // debug
+    console.log('geolocated:', lng, lat)
+
+    // format lng lat values and display them on our 'info' element
+    document.getElementById('info').innerHTML = lng.toFixed(5) + "," + lat.toFixed(5)
+
+})
