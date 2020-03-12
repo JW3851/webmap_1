de5963610099dd096ff1af851be317f256f4be97
JW3851 <62093012+JW3851@users.noreply.github.com>
2020-03-12 05:34:47 -0400

    Update map.js

--- map.js
+++ map.js
@@ -7,7 +7,8 @@ console.log('Loaded map.js')
 mapboxgl.accessToken = 'pk.eyJ1IjoianczODUxIiwiYSI6ImNrN29qcWU2MTBhNWkzZXFxNnQ0OThka2sifQ.tDkhsqVfmARI4pSB5G3CZw'
 let map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v10',
-    center: [-73.96024, 40.80877],
-    zoom: 1
+    style: 'mapbox://styles/brianhouse/cjn0u552b52kr2spdz6yhpqj4',
+    center: [-73.96216,40.80779],
+    zoom: 16,
+    pitch: 45
 })
